dee0c4fb6fa739d6db9dc5298516f9f373017999
cshahIHMC <cshah@ihmc.org>
2025-08-28 17:30:12 -0500

    Added all inputs to the predictor

---
+++ MANN_network_training.py
@@ -0,0 +1,84 @@
+############# Author -Chinmay Shah ##################
+
+# Train Predictor
+## Imports
+import wandb
+from Library import utility
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+
+
+def col_2_extract():
+    
+    cols = ["Pelvis_V_GYROX", "Pelvis_V_GYROY", "Pelvis_V_GYROZ",
+            "LThigh_V_GYROX", "LThigh_V_GYROY", "LThigh_V_GYROZ",
+            "RThigh_V_GYROX", "RThigh_V_GYROY", "RThigh_V_GYROZ",
+            "LShank_V_GYROX", "LShank_V_GYROY", "LShank_V_GYROZ",
+            "RShank_V_GYROX", "RShank_V_GYROY", "RShank_V_GYROZ",
+            "LFoot_V_GYROX", "LFoot_V_GYROY", "LFoot_V_GYROZ",
+            "RFoot_V_GYROX", "RFoot_V_GYROY", "RFoot_V_GYROZ",
+            
+            "Pelvis_V_ACCX", "Pelvis_V_ACCY", "Pelvis_V_ACCZ",
+            "LThigh_V_ACCX", "LThigh_V_ACCY", "LThigh_V_ACCZ",
+            "RThigh_V_ACCX", "RThigh_V_ACCY", "RThigh_V_ACCZ",
+            "LShank_V_ACCX", "LShank_V_ACCY", "LShank_V_ACCZ",
+            "RShank_V_ACCX", "RShank_V_ACCY", "RShank_V_ACCZ",
+            "LFoot_V_ACCX", "LFoot_V_ACCY", "LFoot_V_ACCZ",
+            "RFoot_V_ACCX", "RFoot_V_ACCY", "RFoot_V_ACCZ",
+            "weight"
+            "subject", "condition"
+            ]
+    
+    return cols
+
+def main():
+    
+    # Different Flags
+    # Logging False
+    log_wandB = False
+    train_and_plot = False
+    
+    file_name = "Predictor training Scherpeel Dataset"
+    project_name = "ICRA 2026"
+    
+    # Config the configurations
+    config = {
+        "training_tag": file_name,
+        "project_name": project_name,
+        "epochs": 1,
+        "batch_size": 32,
+        "num_workers": 8,
+        "momentum":0.9,
+        "lr": 1e-4,
+        "dropout": 0.0,
+        "dataset": "IHMC Senorsuit",
+        "seq_length": 201,
+        "inputs": 21,
+        "outputs": 21
+    }
+
+    
+    ## Login to weights and biases and setup the data recording run
+    if log_wandB:
+        wandb.login()
+        project_name = config["project_name"]
+        wandb.init( project=project_name, name= config["training_tag"], config=config)
+    
+    # Data setup
+    data_path = "/home/cshah/workspaces/Sensor-Suit-Motion-Prediction-ICRA-2026/Data/AB01_req_sim_data.csv"
+    df = pd.read_csv(data_path)
+    
+    print("Full df Shape: ", df.shape)
+    
+    cols_2_get = col_2_extract()
+    df_mann = df[cols_2_get]
+    
+    print("Extracted df Shape: ", df_mann.shape)
+
+
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
