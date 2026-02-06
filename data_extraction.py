9bdb581927624e8005af3fdb6a8855754646c528
cshahIHMC <cshah@ihmc.org>
2026-02-06 15:17:06 -0600

    Data extraction for ryna dataset

--- data_extraction.py
+++ data_extraction.py
@@ -4,19 +4,6 @@
 import os
 import pandas as pd
 import csv
-# weight = 78.9 # AB01: 78.9 kg
-# weight = 82.2 # AB02: 82.2 kg
-# weight = 113.5# AB03: 113.5 kg
-# weight = 71.5 # AB05: 71.5 kg
-# weight = 79.1 # AB06: 79.1 kg
-# weight = 62.3 # AB07: 62.3 kg
-# weight = 87.6 # AB08: 87.6 kg 
-# weight = 84.1 # AB09: 84.1 kg
-# weight = 67.5 # AB10: 67.5 kg
-# weight = 65.1 # AB11: 65.1 kg
-# weight = 64.0 # AB12: 64.0 kg
-# weight = 67.6 # AB13: 67.6 kg
-
 
 # Downsampling function - down samples 1000Hz data to 200 Hz
 
@@ -33,19 +20,18 @@ def downSample(df, downsamplingFactor):
     
     return df_downSampled
 
-# Extracts and writes the data to an sql database fromm the keaton scherpeel dataset
-# def extract_write_2_csv(dataDir, SubjectName, dataWriteDir, weight=0.0):
+# Extracts and writes the data to an sql database fromm the Ryan and Christoph Dataset
 def extract_write_2_csv(dataDir, subject_list, dataWriteDir):
 
     df_subjects = pd.DataFrame()
     
-    for SubjectName, weight in subject_list.items():
+    for SubjectName, SubjectSpecificParams in subject_list.items():
         
         # Just to know what subject we are iterating over
         print(SubjectName)
 
         # Get a Subject data path
-        subjectDataPath = dataDir + SubjectName
+        subjectDataPath = dataDir + SubjectName + "MachineLearning/"
 
         # Change the target directory
         os.chdir(subjectDataPath)
@@ -53,61 +39,54 @@ def extract_write_2_csv(dataDir, subject_list, dataWriteDir):
         # Make a list of the folders containing data for different conditions
         conditions_list = [directory for directory in os.listdir() if os.path.isdir(directory)]
 
-
         df_subject = pd.DataFrame()
 
-        for dir in conditions_list:
-
+        for conditionName in conditions_list:
+            
             # Folder path to the conditions
-            conditionFolderPath = subjectDataPath + dir
+            conditionFolderPath = subjectDataPath + conditionName
 
-
+            # List of all the files in the condition folder
             all_files = os.listdir(conditionFolderPath)
 
             # Weed out csv files
             files = [file for file in all_files if file.endswith(".csv")]
-
-            # Initialize a pandas dataframe  for every condition
+            
+            # Initialize a pandas dataframe  
             df_condition = pd.DataFrame()
-
-            # Condition Name
-            conditionName = SubjectName[:-1] + "_" + dir
-
+            
             for file in files:
-
-                # Get the name of the type of data
-                dataName = file[len(conditionName)+1:-4]
-
-                if dataName == "activity_flag" or dataName == "emg" or dataName == "moment" or dataName == "power" or dataName == "grf" or dataName == "moment_filt" or dataName == "imu_real":
-                    continue 
-                
+ 
                 # Data Path
                 dataPath = conditionFolderPath + "/" + file
+        
                 # Read it into a pandas dataframe
 
                 df_data = pd.read_csv(dataPath)
-
-                # Only the EMG Data needs downsampling
-                if dataName == "emg":
-                    downSamplingFactor = 10
-                    df_data = downSample(df_data, downSamplingFactor) 
-
+                
+                # if NAN fill it with 0s
                 df_data.fillna(0, inplace=True)
+                
+                # Add data from the different files in the condition folder on top of each other
+                df_condition = pd.concat([df_condition, df_data], axis=0, ignore_index=True)
+                
+                
 
-                ## Add data from the different data folder to one single data frame right next to each other
-                df_condition = pd.concat([df_condition, df_data], axis=1)
-
-            ## Remove duplicate cols
+            # Remove duplicate cols
             df_condition = df_condition.loc[:, ~df_condition.columns.duplicated()]
 
-            ## Add the name of the condition that we adding data to the database for
-            df_condition['condition'] = dir
+            # Add the name of the condition that we adding data to the database for
+            df_condition['condition'] = conditionName
             df_condition['subject'] = SubjectName[:-1]
-            df_condition['weight'] = weight
+            df_condition['weight'] = SubjectSpecificParams[0]  # Weight is in Kgs
+            df_condition['height'] = SubjectSpecificParams[1]  # Height is in m
+            df_condition['age'] = SubjectSpecificParams[2]  # Age is in years
+            df_condition['sex'] = SubjectSpecificParams[3]  # Sex is M-0 / F-1
 
             # Generate a subject level df
             df_subject = pd.concat([df_subject, df_condition], axis=0, ignore_index=True)      
-        
+            
+
         print(df_subject.shape)
         
         df_subjects = pd.concat([df_subjects, df_subject], axis=0, ignore_index=True)
@@ -125,38 +104,25 @@ def extract_write_2_csv(dataDir, subject_list, dataWriteDir):
 
 
 def main():
-    data_dir = "/home/cshah/workspaces/Sensor-Suit-Motion-Prediction-ICRA-2026/Data/Scheerpeel_Data_set/TestingStuff/"
+    data_dir = "/home/cshah/workspaces/Sensor-Suit-Motion-Prediction-ICRA-2026/Data - Second Skin/OpenSource_Dataset/Data/"
     # sub_Name = "AB01/"
-    data_write_dir = "/home/cshah/workspaces/Sensor-Suit-Motion-Prediction-ICRA-2026/Data/Testing/AB02_jump_req_sim_data.csv"
+    data_write_dir = "/home/cshah/workspaces/Sensor-Suit-Motion-Prediction-ICRA-2026/Data - Second Skin/Testing/5_Subjects_all_data.csv"
     
+    
+    # Subject Information - [Weight(kg), Height(m), Age(yrs), Sex(M-0 / F-1)]]
     sub_list = {
-        # "AB01/": 78.9,
-        "AB02/": 82.2,
-        # "AB03/": 113.5,
-        # "AB05/": 71.5,
-        # "AB06/": 79.1,
-        # "AB07/": 62.3,
-        # "AB08/": 87.6,
-        # "AB09/": 84.1,
-        # "AB10/": 67.5,
-        # "AB11/": 65.1 
-        # "AB12/": 64.0,
-        # "AB13/": 67.6     
+        "AB01/": [86.9, 1.75, 23, 0],   
+        # "AB02/": [84.6, 1.72, 22, 0],
+        # "AB03/": [65.05, 1.765, 32, 0],
+        # "AB04/": [86.4, 1.794, 25, 0],
+        # "AB05/": [87.0, 1.59, 27, 1],
+        # "AB06/": [71.55, 1.868, 24, 0],
+        # "AB08/": [79.0, 1.82, 24, 0],
+        # "AB09/": [58.2, 1.701, 24, 0],
+        # "AB10/": [92.5, 1.825, 27, 0],
+        # "AB11/": [73.5, 1.606, 28, 1] 
         
     }
-    # weight = 78.9 # AB01: 78.9 kg
-    # weight = 82.2 # AB02: 82.2 kg
-    # weight = 113.5# AB03: 113.5 kg
-    # weight = 71.5 # AB05: 71.5 kg
-    # weight = 79.1 # AB06: 79.1 kg
-    # weight = 62.3 # AB07: 62.3 kg
-    # weight = 87.6 # AB08: 87.6 kg 
-    # weight = 84.1 # AB09: 84.1 kg
-    # weight = 67.5 # AB10: 67.5 kg
-    # weight = 65.1 # AB11: 65.1 kg
-    # weight = 64.0 # AB12: 64.0 kg
-    # weight = 67.6 # AB13: 67.6 kg
-
     
     # extract_write_2_csv(data_dir,sub_Name, data_write_dir, weight=78.90)
     extract_write_2_csv(data_dir,sub_list, data_write_dir)
